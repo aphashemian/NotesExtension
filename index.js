c2b6d55cb74dd0304fee1ecc6ccae176683a61ea
aphashemian <amir.p.hashemian@gmail.com>
2022-01-08 23:05:21 -0600

    modded.

--- index.js
+++ index.js
@@ -4,8 +4,35 @@
 let myInputs = [];
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function() {
-  myInputs.push(inputEl.textContent);
-  console.log("pressed");
-})
+  myInputs.push(inputEl.value);
+  console.log(myInputs);
+  inputEl.value = "";
+  render();
+});
+
+
+function render()
+{
+  let listItems="";
+  for (let i = 0; i < myInputs.length; i++)
+  {
+    // create element.
+    // set text cntent.
+    // append to ul.
+    //ulEl.innerHtml += "<li>" + myInputs[i] + "</li>"
+    // const li = document.createElement("li");
+    // li.textContent = myInputs;
+    // ulEl.append(li);
+
+    myInputs += `
+      <li>
+        <a target = '_blank' href='${myInputs[i]}'>
+          ${myInputs[i]}
+        </a>
+      </li>
+    `
+    ;
+}
